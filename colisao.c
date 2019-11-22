9a8af7240b49d63a84e15a61d88192ca274c3870
steinglk <soloqstein@outlook.com>
2019-11-22 15:36:41 -0300

    colisão em uma tabela hash

--- colisao.c
+++ colisao.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 //Estrutura base do nó->
 struct Node
@@ -21,7 +22,25 @@ int hash(int codigo)
   }
   return chave;
 }
+//Função pra printar o nó na tela.
+void displayLL2(node *p)
+{
+    p = p->proximo;
+    printf("Mostrando a lista:\n");
+    if(p)
+    {
+        do
+        {
+            printf(" %d", p->valor);
+            p = p->proximo;
+        }
+        while(p);
+        printf("\n\n");
 
+    }
+    else
+        printf("Lista vazia.\n\n");
+}
 // imprime a lista na posicao da chave hash
 void displayLL(int chave, node *p)
 {
@@ -49,18 +68,36 @@ void insere_no_final(node *tabela, int info){
     novo->proximo = NULL;
     tabela->proximo = novo;
 }
+void add(int chave, int codigo, node *tabela_hash) {
+  bool n;
+  if (tabela_hash)
+  {
+    n = true;
+  }
+  else
+    n = false;
+  if(n == true) {
+    insere_no_final(tabela_hash, codigo);
 
+  }
+  else {
+    tabela_hash = (node *)malloc(sizeof(node));
+    tabela_hash->valor = codigo;
+    tabela_hash->proximo = NULL;
+  }
+
+}
 int main(void)
-{ 
+{
   int tamanho_da_tabela_hash = 10;
   node *tabela_hash[tamanho_da_tabela_hash];
   for (int i = 0; i < tamanho_da_tabela_hash; i++)
   {
     tabela_hash[i] = NULL;
   }
-  
+
   // criando o primeiro nodo na tabela hash
-  int codigo1 = 100112345;
+  int codigo1 = 100112375;
   int chave1 = hash(codigo1);
 
   // criando o primeiro nodo na tabela hash
@@ -69,16 +106,41 @@ int main(void)
     tabela_hash[chave1]->valor = codigo1;
     tabela_hash[chave1]->proximo = NULL;
   }
-  else 
-  
-  // informacoes para inserir o segundo nodo
-  int codigo2 = 100154325;
-  int chave2 = hash(codigo2);
+  else
+    insere_no_final(tabela_hash[chave1], codigo1);
 
-  // criando o segundo nodo na tabela hash
-  tabela_hash[chave2] = (node *)malloc(sizeof(node));
-  tabela_hash[chave2]->valor = codigo2;
-  tabela_hash[chave2]->proximo = NULL;
+    // // Informações para o segundo nodo
+     int codigo2 = 100112345;
+     int chave2 = hash(codigo2);
+     add(chave2, codigo2, tabela_hash[chave2]);
+
+     int codigo3 = 100112344;
+     int chave3 = hash(codigo3);
+     if(tabela_hash[chave3] == NULL) {
+       tabela_hash[chave3] = (node *)malloc(sizeof(node));
+       tabela_hash[chave3]->valor = codigo3;
+       tabela_hash[chave3]->proximo = NULL;
+     }
+     else
+       insere_no_final(tabela_hash[chave3], codigo3);
+    int codigo4 = 100113804;
+    int chave4 = hash(codigo4);
+       if(tabela_hash[chave4] == NULL) {
+         tabela_hash[chave4] = (node *)malloc(sizeof(node));
+         tabela_hash[chave4]->valor = codigo4;
+         tabela_hash[chave4]->proximo = NULL;
+       }
+       else
+         insere_no_final(tabela_hash[chave4], codigo4);
+    //
+    // // criando o primeiro nodo na tabela hash
+    // if(tabela_hash[chave1] == NULL) {
+    //   tabela_hash[chave1] = (node *)malloc(sizeof(node));
+    //   tabela_hash[chave1]->valor = codigo1;
+    //   tabela_hash[chave1]->proximo = NULL;
+    // }
+    // else
+    //   insere_no_final(tabela_hash[chave1], codigo1);
 
   // imprimindo toda a estrutura
   for (int i = 0; i < 10; i++)
